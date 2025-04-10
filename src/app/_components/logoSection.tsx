112492f7616cd87161fb3d2a42390e29300cc8ca
amanjoshi <aman.joshi1111@gmail.com>
2025-04-11 00:44:25 +0530

    did some juggad for logo in dark more, right approach their should be another logo for dark more, even better create a custom logo which works for both the mode

--- src/app/_components/logoSection.tsx
+++ src/app/_components/logoSection.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function LogoSection() {
     return <Link className="flex items-center gap-2 text-3xl font-bold w-60 cursor-pointer" href={"/"}>
-        <Image src={Logo} alt="Logo" className="size-10 rotate-90"></Image>
+        <Image src={Logo} alt="Logo" className="size-10 rotate-90 dark:border-white dark:border-2 dark:rounded-full"></Image>
         <p>Easy<span className="text-orange-500">Billing</span></p>
     </Link>
 }
