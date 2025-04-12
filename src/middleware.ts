23b0cbce67ed0962b20a44b9281e4d06a306e2ae
amanjoshi <aman.joshi1111@gmail.com>
2025-04-12 23:31:40 +0530

    middleware flow fixed now onboarding remaining

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,39 +16,24 @@ export default auth(async (req) => {
 
     const { nextUrl } = req;
     const pathname = nextUrl.pathname;
-
-    //If route is public, return
-    if (publicRoutes.includes(pathname) || authRoutes.includes(pathname)) {
-        return NextResponse.next();
-    }
-    //If it's auth api
-    if (pathname.includes("/api/auth")) {
-        return NextResponse.next();
-    }
     const isLoggedIn = !!req.auth;
     const isAuthRoute = authRoutes.includes(pathname);
 
     console.log({ pathname, isLoggedIn, isAuthRoute });
 
     if (!isLoggedIn) {
-        return NextResponse.redirect(new URL("/login", nextUrl.origin));
+        if (isAuthRoute || publicRoutes.includes(pathname) || pathname.includes("/api/auth")) {
+            return NextResponse.next();
+        } else {
+            return NextResponse.redirect(new URL("/login", nextUrl.origin));
+        }
     }
-
-    //If logged in and it's authRoute(page), redirect to '/' page 
-    // No need to show /login, /verify
-    if (isLoggedIn && isAuthRoute) {
-        return NextResponse.redirect(new URL("/dashboard", nextUrl.origin));
-    }
-
-    //if logged in and not a authRoute(page), good to go
-    if (isLoggedIn && !isAuthRoute) {
-        // const session = await auth();
-        //if user is not onboarded just allow him "/" screen
-        // if(!session?.user.isOnboarded && (!publicRoutes.includes(pathname)))
-        // if (!session?.user.isOnboarded && !(pathname === "/onboarding" || pathname === "/")) {
-        //     return NextResponse.redirect(new URL("/onboarding", nextUrl.origin));
-        // }
-        return NextResponse.next();
+    if (isLoggedIn) {
+        if (isAuthRoute) {
+            return NextResponse.redirect(new URL("/", nextUrl.origin));
+        } else {
+            return NextResponse.next();
+        }
     }
 })
 
