935c9b6aae23c0eae1927f37240441a62e184af7
amanjoshi <aman.joshi1111@gmail.com>
2025-04-13 15:25:04 +0530

    seed.ts done

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,33 +1,130 @@
+// import { createInvoiceFormSchema, CreateInvoiceFormSchema } from "@/app/(dashboard)/invoices/type";
 import { prisma } from "../src/db";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
-async function seed() {
-    let total: number = 0;
-    const invoiceData = await prisma.invoice.findMany();
-    console.log("Total invoice present : ", invoiceData.length);
-    for (let i = 0; i < invoiceData.length; i++) {
-        const invoice = invoiceData[i];
-        const minAmount = Math.min(Number(invoice.total), Number(invoice.subTotal));
-        total += minAmount;
-        // if (minAmount > 10000) {
-        //     await prisma.invoice.update({
-        //         where: {
-        //             id: invoice.id
-        //         },
-        //         data: {     
-        //             currencyId: 3,
-        //             total: minAmount / 100,
-        //             subTotal: minAmount / 100,
-        //             discount: 0
-        //         }
-        //     })
-        //     console.log("Updated for invoiceNumber : ", invoice.invoiceNumber);
-        // }
+async function seedInvoices() {
+
+    //1. take user id
+    const userId = "cm9ehfjtd0000o6j6w2p2uqx2";
+    for (let i = 0; i < 100; i++) {
+        const createdAt = randomDateInRange(new Date(2023, 0, 1), new Date(2025, 2, 1));
+        const dueDate = randomDateInRange(new Date(2024, 0, 1), new Date(2025, 11, 1));
+        const randomReciever = recieverInfoArray[Math.floor((Math.random() * recieverInfoArray.length))];
+        const items = getRandomItems();
+        const subTotal = items.reduce((acc, item) => {
+            return acc + item.amount;
+        }, 0);
+
+        const validatedData = {
+            invoiceName: "invoce " + i,
+            dueDate: dueDate,
+            currency: 2,
+            fromName: senderInfo.name,
+            fromEmail: senderInfo.email,
+            fromAddress: senderInfo.address,
+            toName: randomReciever.name,
+            toEmail: randomReciever.email,
+            toAddress: randomReciever.address,
+            items: items,
+            subTotal: subTotal,
+            discount: 0,
+            total: subTotal,
+            status: "PENDING"
+        }
+
+        // const { success, data: validatedData } = createInvoiceFormSchema.safeParse(invoiceData);
+
+        const data = await prisma.invoice.create({
+            data: {
+                invoiceName: validatedData.invoiceName,
+                dueDate: validatedData.dueDate,
+                fromName: validatedData.fromName,
+                fromEmail: validatedData.fromEmail,
+                fromAddress: validatedData.fromAddress,
+                toName: validatedData.toName,
+                toEmail: validatedData.toEmail,
+                toAddress: validatedData.toAddress,
+                items: {
+                    create: validatedData.items.map((item) => ({
+                        description: item.description,
+                        quantity: item.quantity,
+                        rate: item.rate,
+                        amount: item.amount
+                    })),
+                },
+                subTotal: validatedData.subTotal,
+                discount: validatedData.discount,
+                total: validatedData.total,
+                createdAt: createdAt,
+                user: {
+                    connect: {
+                        id: userId
+                    }
+                },
+                currency: {
+                    connect: {
+                        id: validatedData.currency
+                    }
+                }
+            },
+        });
     }
-    console.log({ total });
+}   
+
+function getRandomItems() {
+    const totalItems = Math.floor(Math.random() * 5) + 1;
+    const items = [];
+    for (let i = 0; i < totalItems; i++) {
+        const description = `item ${i}`;
+        const quantity = Math.floor(Math.random() * 5) + 1;
+        const rate = Math.floor(Math.random() * 200) + 100;
+        const amount = quantity * rate;
+
+        items.push({ description, quantity, rate, amount });
+    }
+    return items;
 }
 
-seed()
+const randomDateInRange = (lowDate: Date, upperDate: Date) => {
+    const randomDate = Math.floor(Math.random() * (upperDate.getTime() - lowDate.getTime())) + lowDate.getTime();
+    return new Date(randomDate);
+}
+
+const senderInfo = {
+    name: "aman joshi",
+    email: "aman.joshi1111@gmail.com",
+    address: "aman joshi address"
+}
+
+const recieverInfoArray = [
+    {
+        name: "surya",
+        email: "aman.joshi1111@gmail.com",
+        address: "aman joshi address"
+    },
+    {
+        name: "rahul gandhi",
+        email: "rahul.gandhi@gmail.com",
+        address: "rahul gandhi address"
+    },
+    {
+        name: "amit pandey",
+        email: "aman.joshi1111@gmail.com",
+        address: "aman joshi address"
+    },
+    {
+        name: "nishant",
+        email: "aman.joshi1111@gmail.com",
+        address: "aman joshi address"
+    },
+    {
+        name: "nitin",
+        email: "aman.joshi1111@gmail.com",
+        address: "aman joshi address"
+    }
+]
+
+seedInvoices()
     .then(() => {
         console.log("DATA SEEDED SUCCESSFULLY");
     })
